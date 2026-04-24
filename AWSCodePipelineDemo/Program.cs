c7036dd1b0de77072d68acd417348e4bcaa86001
Maksym Medvid <medvmaks6@gmail.com>
2026-04-24 20:12:05 +0300

    Hardcode port 5000 for AWS Elastic Beanstalk

--- AWSCodePipelineDemo/Program.cs
+++ AWSCodePipelineDemo/Program.cs
@@ -1,24 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// ПРИМУСОВО вказуємо порт 5000, щоб AWS Elastic Beanstalk був задоволений
+builder.WebHost.UseUrls("http://*:5000");
+
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(); // ЗМІНЕНО ТУТ
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();   // ЗМІНЕНО ТУТ
-    app.UseSwaggerUI(); // ЗМІНЕНО ТУТ
-}
+// Вмикаємо Swagger для всіх середовищ (корисно для перевірки)
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
-// Наш тестовий ендпоінт
+// Наш головний тестовий ендпоінт
 app.MapGet("/", () => "Hello World! .NET 8 on Debian");
 
 app.Run();
