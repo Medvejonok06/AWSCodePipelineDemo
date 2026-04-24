a0086e3245a7970935161accd5ff442c12cde133
Maksym Medvid <medvmaks6@gmail.com>
2026-04-24 19:41:26 +0300

    Fix NuGet packages for .NET 8

--- AWSCodePipelineDemo/Program.cs
+++ AWSCodePipelineDemo/Program.cs
@@ -1,25 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(); // ЗМІНЕНО ТУТ
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();   // ЗМІНЕНО ТУТ
+    app.UseSwaggerUI(); // ЗМІНЕНО ТУТ
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
-app.MapGet("/", () => "Hello World! .NET 9 on Debian");
+// Наш тестовий ендпоінт
+app.MapGet("/", () => "Hello World! .NET 8 on Debian");
 
 app.Run();
